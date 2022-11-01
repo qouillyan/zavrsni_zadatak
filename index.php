5274f5d04f3115c4304cb7d67e97c9edeef91c2c
qouillyan <stojakovicdragan98@gmail.com>
2022-11-01 11:40:46 +0100

    Mali commit u toku izrade zadatka

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 <!doctype html>
 <html lang="en">
 <head>
-
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
@@ -20,14 +19,10 @@
 </head>
 
 <body>
-
-<?php include('db.php') ?>
-
-<?php include('header.php') ?>
-
-<?php include('posts.php') ?>
-
-<?php include('footer.php') ?>
-
+    <?php include('db.php') ?>
+    <?php include('header.php') ?>
+    <?php include('posts.php') ?>
+    <?php include('footer.php') ?>
 </body>
+
 </html>
--- single-post.php
+++ single-post.php
@@ -1,33 +1,60 @@
 <!doctype html>
 <html lang="en">
-<head>
 
+<head>
     <meta charset="utf-8">
-    <!-- <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="../../../../favicon.ico"> -->
+    <link rel="icon" href="../../../../favicon.ico">
 
-    <title>Posts</title>
+    <title>Vivify Blog</title>
 
     <!-- Bootstrap core CSS -->
-    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
 
     <!-- Custom styles for this template -->
-    <!-- <link href="styles/blog.css" rel="stylesheet"> -->
+    <link href="styles/blog.css" rel="stylesheet">
+    <link href="styles/styles.css" rel="stylesheet">
 </head>
 
 <body>
-<?php include('db.php') ?>
-    <div>
-        <?php include('header.php') ?>
-    </div>
-    <div>
-        <?php include('aside.php') ?>
-    </div>
-    <div>
-        <?php include('footer.php') ?>
-    </div>
-</body>
 
+<?php include('db.php') ?>
+
+    <?php
+        $sql = "SELECT * FROM posts";
+        $singlePost = getData($sql, $connection);
+    ?>
+
+<?php include('header.php') ?>
+    
+<main role="main" class="container">
+    
+    <div class="row">
+        
+            <div class="col-sm-8 blog-main">
+            
+                <div class="blog-post">
+                    <a href="single-post.php"><h2 class="blog-post-title"> <?php echo($singlePost['title']) ?></h2></a>
+                    <p class="blog-post-meta"><?php echo($singlePost['created_at']) ?> by <a href="#"> <?php echo($singlePost['author']) ?></a></p>
+                    <p><?php echo($singlePost['body']) ?></p>
+                </div>
+
+            </div>  
+            
+            <?php include('aside.php') ?>
+        
+        <nav class="blog-pagination">
+            <a class="btn btn-outline-primary" href="#">Older</a>
+            <a class="btn btn-outline-secondary disabled" href="#">Newer</a>
+        </nav>
+        
+    </div>
+    
+</main>
+
+<?php include('footer.php') ?>
+
+</body>
 </html>
